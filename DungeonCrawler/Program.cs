425ec9fc522eca71c9326af060a1261f08e5372c
ricardotenorio <ricardo.heber@gmail.com>
2021-10-02 10:08:20 -0300

    Implemented console user interface

--- DungeonCrawler/Program.cs
+++ DungeonCrawler/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DungeonCrawler.UI;
+using DungeonCrawler.UI.Terminal;
+using System;
+using System.Collections.Generic;
 
 namespace DungeonCrawler
 {
@@ -6,7 +9,28 @@ namespace DungeonCrawler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IUserInterface ui = new TerminalUI();
+            int[,] state = new int[20, 20];
+            Queue<string> msg = new Queue<string>();
+
+            state[0, 0] = 1;
+            state[1, 10] = 5;
+            state[10, 1] = 4;
+            state[15, 15] = 3;
+            state[17, 11] = 2;
+            state[19, 19] = 6;
+
+            Dictionary<string, int> stats = new Dictionary<string, int>();
+            stats.Add("hp", 25);
+            stats.Add("damage", 1);
+            stats.Add("score", 15);
+
+            msg.Enqueue("this is a test");
+            msg.Enqueue("another test");
+
+            ui.Draw(state, stats, msg);
+
+            Console.WriteLine(msg.Count);
         }
     }
 }
---
+++ DungeonCrawler/UI/Terminal/TerminalUI.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawler.UI.Terminal
+{
+    class TerminalUI : IUserInterface
+    {
+        private readonly string _border = "==============================================";
+        private Dictionary<char, ConsoleColor> _charColors = new Dictionary<char, ConsoleColor>()
+            {
+                { 'H', ConsoleColor.Cyan },
+                { 'M', ConsoleColor.Red },
+                { 'B', ConsoleColor.Red },
+                { 'P', ConsoleColor.Green },
+                { 'W', ConsoleColor.Yellow },
+                { 'D', ConsoleColor.Blue },
+                { '0', ConsoleColor.Gray }
+            };
+
+        public void Draw(int[,] floorState, Dictionary<string, int> heroStats, Queue<string> messages)
+        {
+            Console.Clear();
+            DrawStats(heroStats);
+            DrawFloor(floorState);
+            DrawCommandHints();
+            DrawMessages(messages);
+        }
+
+        private void DrawStats(Dictionary<string, int> heroStats)
+        {
+            Console.WriteLine(_border);
+            Console.Write($"Hero HP: {heroStats["hp"]} ");
+            Console.Write($"Hero Damage: {heroStats["damage"]} ");
+            Console.WriteLine($"Hero Score: {heroStats["score"]}");
+            Console.WriteLine(_border);
+        }
+
+        private void DrawFloor(int[,] floorState)
+        {
+            char tile;
+
+            for(int i = 0; i < floorState.GetLength(0); i++)
+            {
+                Console.Write("  ");
+                for (int j = 0; j < floorState.GetLength(1); j++)
+                {
+                    tile = floorState[i, j] switch
+                    {
+                        1 => 'H',
+                        2 => 'M',
+                        3 => 'B',
+                        4 => 'P',
+                        5 => 'W',
+                        6 => 'D',
+                        _ => '0',
+                    };
+
+                    Console.ForegroundColor = _charColors[tile];
+                    Console.Write(tile + " ");
+                }
+
+                Console.WriteLine();
+            }
+                
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+        private void DrawCommandHints()
+        {
+            Console.WriteLine(_border);
+
+            Console.Write($"[A] to move left.   ");
+            Console.WriteLine($"[D] to move right.");
+
+            Console.Write($"[W] to move up.     ");
+            Console.WriteLine($"[S] to move down.");
+
+            Console.Write($"[Space] to attack.  ");
+            Console.WriteLine($"[Esc] to exit.");
+            
+            Console.WriteLine(_border);
+        }
+
+        private void DrawMessages(Queue<string> messages)
+        {
+            while (messages.Any())
+            {
+                Console.WriteLine(messages.Dequeue());
+            }
+
+            Console.WriteLine(_border);
+        }
+    }
+}
