04bdcd99a2508e044d6046205c0197ad3886165f
ricardotenorio <ricardo.heber@gmail.com>
2021-10-05 14:32:13 -0300

    implemented weapon and potion classes

---
+++ DungeonCrawler/GameObjects/Item/Potion.cs
@@ -0,0 +1,24 @@
+﻿using DungeonCrawler.GameObjects.Character.Playable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawler.GameObjects.Item
+{
+    class Potion : IItem
+    {
+        public int EffectValue { get; private set; }
+
+        public Potion(int effectValue)
+        {
+            this.EffectValue = effectValue;
+        }
+
+        public void ApplyEffect(Hero targetCharacter)
+        {
+            targetCharacter.RecoverHP(EffectValue);
+        }
+    }
+}
---
+++ DungeonCrawler/GameObjects/Item/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using DungeonCrawler.GameObjects.Character.Playable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawler.GameObjects.Item
+{
+    class Weapon : IItem
+    {
+        public int EffectValue { get; private set; }
+
+        public Weapon(int effectValue)
+        {
+            this.EffectValue = effectValue;
+        }
+
+        public void ApplyEffect(Hero targetCharacter)
+        {
+            targetCharacter.RaiseAttack(EffectValue);
+        }
+    }
+}
